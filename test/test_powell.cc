f8ee7a610a9428395df8fec0e8ef23901a153c01
yytdfc <fuchen@foxmail.com>
2018-06-22 17:15:07 +0800

    modify the powell test

--- test/test_powell.cc
+++ test/test_powell.cc
@@ -1,66 +1,21 @@
+#include <gtest/gtest.h>
 #include "optimizer/powell.h"
+#include "doe.h"
 #include "some_func.h"
 using namespace MetaOpt;
 using namespace std;
 
-int main() {
-  // (n_dim, n_obj, n_con, func, upper, lower)
-  const int dim = 2;
+TEST(metaopt, doe) {
+  const int               dim = 2;
   MetaOpt::Powell<double> a(dim, rosenbrock<double, dim>);
-  auto s = a.sample();
-  a.opt(s);
-  // auto s = a.sample();
-  std::cout << s << std::endl;
+  auto r = Doe<double, MC>::gen(1, dim, vector<double>(dim, 0),
+                                vector<double>(dim, 2))[0];
+  std::cout << r << std::endl;
+  a.opt(r);
+  std::cout << r << std::endl;
   std::cout << a.n_evaluation_ << std::endl;
-  // a.evaluate(s);
-  // std::cout << s << std::endl;
-  // MetaOpt::Ga<double> a(2, 1, 0, rosenbrock, vector<double>{0, 0},
-  // vector<double>{2, 0}, 10,0.1,0.1);
-  // MetaOpt::<float> a(2);
-
-  // MetaOpt::Optimizer<double> a(2, 1, 0, rosenbrock, vector<double>{-2, -2},
-  //                         vector<double>{2, 2});
-
-  // MetaOpt::Sample<double> a(vector<double>{2,
-  // 2},vector<double>(2,0),vector<double>());
-  // // MetaOpt::Sample<double> aa(vector<double>{2,
-  // 2},vector<double>(2,0),vector<double>()); MetaOpt::Sample<double>
-  // aa(move(vector<double>{2,
-  // 2}),move(vector<double>(2,0)),move(vector<double>()));
-  // // MetaOpt::Sample<double> ab(vector<double>{2,
-  // 2},vector<double>(2,0),vector<double>()); LOG(INFO) << a; LOG(INFO) << aa;
-  // MetaOpt::Sample<double> b(a);
-  // MetaOpt::Sample<double> d(move(aa));
-  // LOG(INFO) << a;
-  // LOG(INFO) << aa;
-  // LOG(INFO) << a.x_.size();
-  // LOG(INFO) << aa.x_.size();
-  // a.x_[0] = 3;
-  // // aa.x_[0] = 3;
-  // LOG(INFO) << a;
-  // LOG(INFO) << aa;
-  // LOG(INFO) << "(move(Sample()))";
-  // MetaOpt::Sample<double> dss(move(MetaOpt::Sample<double>(
-  //     vector<double>{2, 2}, vector<double>(2, 0), vector<double>())));
-  // LOG(INFO) << "(Sample())";
-  // MetaOpt::Sample<double> cc(vector<double>{2, 3}, vector<double>(2, 0),
-  //                            vector<double>());
-  // MetaOpt::Sample<double> dsss(cc);
-  // cc.x_[0] = 3;
-  // LOG(INFO) << dss;
-  // LOG(INFO) << cc;
-  // LOG(INFO) << dsss;
-  // cc = dss;
-  // LOG(INFO) << dss;
-  // LOG(INFO) << cc;
-  // LOG(INFO) << dsss;
-  // dss.x_[0] = 9;
-  // LOG(INFO) << dss;
-  // LOG(INFO) << cc;
-  // LOG(INFO) << dsss;
-  // cc = move(dss);
-  // LOG(INFO) << dss;
-  // LOG(INFO) << cc;
-  // LOG(INFO) << dsss;
-  // return 0;
+  EXPECT_LE(fabs(r[dim]), 1e-6);
+  for (auto i : exd::range::range(dim)) {
+    EXPECT_LE(fabs(r[i] - 1), 1e-3);
+  }
 }
